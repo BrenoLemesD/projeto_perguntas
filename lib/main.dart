62721fe67993d82d2b15601ee8784cb9b4e45488
Breno Otávio Lemes Diamantino <113796649+BrenoLemesD@users.noreply.github.com>
2023-09-11 20:12:46 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,69 @@
 import 'package:flutter/material.dart';
-import './questionario.dart';
-import './resultado.dart';
 
-void main() => runApp(const PerguntaApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaSelecionada = 0;
-  var _pontuacaoTotal = 0;
-  final _perguntas = const [
-    {
-      'texto':
-          'Responda o questionario a seguir para ver se você conhece o Breno',
-      'respostas': [
-        {'texto': 'Claro', 'pontuacao': 10},
-      ],
-    },
-    {
-      'texto': 'Qual é a cor favorita do Breno?',
-      'respostas': [
-        {'texto': 'Preto', 'pontuacao': 0},
-        {'texto': 'Vermelho', 'pontuacao': 5},
-        {'texto': 'Verde', 'pontuacao': 0},
-        {'texto': 'Branco', 'pontuacao': 0},
-      ],
-    },
-    {
-      'texto': 'Qual é o animal de estimação favorito dele?',
-      'respostas': [
-        {'texto': 'Coelho', 'pontuacao': 0},
-        {'texto': 'Gato', 'pontuacao': 5},
-        {'texto': 'Cobra', 'pontuacao': 0},
-        {'texto': 'Leão', 'pontuacao': 0},
-      ],
-    },
-    {
-      'texto': 'E o esporte você sabe?',
-      'respostas': [
-        {'texto': 'Basquete', 'pontuacao': 5},
-        {'texto': 'Futebol', 'pontuacao': 0},
-        {'texto': 'Vôlei', 'pontuacao': 0},
-        {'texto': 'Natação', 'pontuacao': 0},
-      ],
-    }
-  ];
-
-  void _responder(int pontuacao) {
-    if (temPerguntaSelecionada) {
-      setState(() {
-        _perguntaSelecionada++;
-        _pontuacaoTotal += pontuacao;
-      });
-    }
-  }
-
-  void _reiniciarTudo() {
-    setState(() {
-      _perguntaSelecionada = 0;
-      _pontuacaoTotal = 0;
-    });
-  }
-
-  bool get temPerguntaSelecionada {
-    return _perguntaSelecionada < _perguntas.length;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: temPerguntaSelecionada
-            ? Questionario(
-                perguntas: _perguntas,
-                perguntaSelecionada: _perguntaSelecionada,
-                quandoResponder: _responder,
-              )
-            : Resultado(_pontuacaoTotal, _reiniciarTudo),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class PerguntaApp extends StatefulWidget {
-  const PerguntaApp({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  _PerguntaAppState createState() {
-    return _PerguntaAppState();
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
